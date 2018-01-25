a36262bdb9a618189ea05d2d50a4b215aad9cb8c
Vikas <veikas@mangalyaan>
2018-01-25 19:39:36 +0100

    LC shouldnt be in header file. remove main in the library due to linking error

--- cpp/evaluate_flow.cpp
+++ cpp/evaluate_flow.cpp
@@ -379,3 +379,31 @@ bool eval (string result_sha,Mail* mail) {
   // success
 	return true;
 }
+
+int32_t main_example (int32_t argc,char *argv[]) {
+
+  // we need 2 or 4 arguments!
+  if (argc!=2 && argc!=4) {
+    cout << "Usage: ./eval_flow result_sha [user_sha email]" << endl;
+    return 1;
+  }
+
+  // read arguments
+  string result_sha = argv[1];
+
+  // init notification mail
+  Mail *mail;
+  if (argc==4) mail = new Mail(argv[3]);
+  else         mail = new Mail();
+  mail->msg("Thank you for participating in our evaluation!");
+
+  // run evaluation
+  bool success = eval(result_sha,mail);
+  if (argc==4) mail->finalize(success,"flow",result_sha,argv[2]);
+  else         mail->finalize(success,"flow",result_sha);
+
+  // send mail and exit
+  delete mail;
+  return 0;
+}
+
